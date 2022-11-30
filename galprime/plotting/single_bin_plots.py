7cadae2275bb17c80092e5b3c2c59c94f158ee20
hsouch <harrison.souchereau@yale.edu>
2022-11-30 12:54:13 -0500

    Removed outdated tbridge import

--- galprime/plotting/single_bin_plots.py
+++ galprime/plotting/single_bin_plots.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 from astropy.table import Table
 from numpy import copy, log10
-import tbridge
+import galprime
 
 
 def adjust_profile(x, y, m_0=27, arcconv=0.168):
@@ -12,49 +12,49 @@ def adjust_profile(x, y, m_0=27, arcconv=0.168):
     return x_new, y_new
 
 
-def single_bin_plot(table_arrays, colours=None, ind_profile_alpha=1, medians=False, to_sb=True,
-                    bootstraps=False, ylim=(30, 20),
-                    xlabel=None, ylabel=None, output="", dpi=150):
+# def single_bin_plot(table_arrays, colours=None, ind_profile_alpha=1, medians=False, to_sb=True,
+#                     bootstraps=False, ylim=(30, 20),
+#                     xlabel=None, ylabel=None, output="", dpi=150):
 
-    plt.figure(figsize=(8, 6))
+#     plt.figure(figsize=(8, 6))
 
-    for i in range(0, len(table_arrays)):
-        for j in range(0, len(table_arrays[i])):
-            this_prof = table_arrays[i][j]
+#     for i in range(0, len(table_arrays)):
+#         for j in range(0, len(table_arrays[i])):
+#             this_prof = table_arrays[i][j]
 
-            sma, intens = this_prof["sma"], this_prof["intens"]
-            sma, intens = adjust_profile(sma, intens)
+#             sma, intens = this_prof["sma"], this_prof["intens"]
+#             sma, intens = adjust_profile(sma, intens)
 
-            plt.plot(sma, intens, c=colours[i], alpha=ind_profile_alpha, lw=1)
+#             plt.plot(sma, intens, c=colours[i], alpha=ind_profile_alpha, lw=1)
 
-        if medians:
-            interps = tbridge.as_interpolations(table_arrays[i])
-            median_sma, median_interp = tbridge.get_median(interps, tbridge.bin_max(table_arrays[i]))
-            median_intens = median_interp(median_sma)
+#         if medians:
+#             interps = tbridge.as_interpolations(table_arrays[i])
+#             median_sma, median_interp = tbridge.get_median(interps, tbridge.bin_max(table_arrays[i]))
+#             median_intens = median_interp(median_sma)
 
-            if to_sb:
-                median_sma, median_intens = adjust_profile(median_sma, median_intens)
+#             if to_sb:
+#                 median_sma, median_intens = adjust_profile(median_sma, median_intens)
 
-            plt.plot(median_sma, median_intens, lw=3, c=colours[i])
+#             plt.plot(median_sma, median_intens, lw=3, c=colours[i])
 
-    if ylim is None:
-        pass
-    else:
-        plt.ylim(ylim[0], ylim[1])
-    plt.xlim(1,)
-    plt.xscale("log")
+#     if ylim is None:
+#         pass
+#     else:
+#         plt.ylim(ylim[0], ylim[1])
+#     plt.xlim(1,)
+#     plt.xscale("log")
 
-    if xlabel is not None:
-        plt.xlabel(xlabel)
-    if ylabel is not None:
-        plt.ylabel(ylabel)
+#     if xlabel is not None:
+#         plt.xlabel(xlabel)
+#     if ylabel is not None:
+#         plt.ylabel(ylabel)
 
-    plt.tight_layout()
+#     plt.tight_layout()
 
-    if output == "":
-        plt.show()
-    else:
-        plt.savefig(output, dpi=dpi)
+#     if output == "":
+#         plt.show()
+#     else:
+#         plt.savefig(output, dpi=dpi)
 
 
 def single_prof(x, y, error=None):
