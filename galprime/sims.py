03dda35e6de64edbf62660284c8ace67ec6cb5f0
hsouch <harrison.souchereau@yale.edu>
2022-12-03 21:11:23 -0500

    Fixed (I think) multiprocessing w/ Pebble

--- galprime/sims.py
+++ galprime/sims.py
@@ -1,6 +1,6 @@
 import galprime
 import numpy as np
-import os
+import os, time
 
 from matplotlib import pyplot as plt
 
@@ -15,16 +15,19 @@ import galprime
 from pebble import ProcessPool
 from concurrent.futures import TimeoutError
 
-from tqdm import tqdm
 
 class GalPrimeError(Exception):
     pass
 
 
 class GPrime():
-    """ Class to create and run GalPrime simulations """
+    """ Class to manage GalPrime simulations """
     
-    def __init__(self, config=None, psfs=None, backgrounds=None, mag_kde=None):
+    def __init__(self, config=None, psfs=None, backgrounds=None, mag_kde=None, 
+                 table_names=["ID","IMG", "BG_INDEX", "X", "Y", "RA", "DEC", "MAGS", "R50S", "NS", "ELLIPS", "MASS_MED",
+                              "ZPHOT", "sfProb", "I_R50", "PA", "R50_PIX", "BG_MEAN", "BG_MED", "BG_STD",
+                              "NSEG_BGA", "CENT_BGA", "N_MASKED_BGA", "P_MASKED_BGA", "NSEG_BGSUB", "CENT_BGSUB", 
+                              "N_MASKED_BGSUB", "P_MASKED_BGSUB", "T_ELPSD"]):
 
         if config is None:
             self.config = galprime.default_config_params()
@@ -41,17 +44,16 @@ class GPrime():
         self.backgrounds = backgrounds
         self.mag_kde = mag_kde
 
+        self.table_names = table_names
+        
+
     def load_bins(self):
         self.binned_objects = galprime.bin_catalog(self.config)
     
     
     def pipeline(self, max_bins=None, mag_kde=None, process_method=None,
-                progress_bar=False, debug=False, 
-                 table_names=["ID","IMG", "X", "Y", "RA", "DEC", "MAGS", "R50S", "NS", "ELLIPS", "MASS_MED",
-                              "ZPHOT", "sfProb", "I_R50", "PA", "R50_PIX", "BG_MEAN", "BG_MED", "BG_STD",
-                              "NSEG_BGA", "CENT_BGA", "N_MASKED_BGA", "P_MASKED_BGA", "NSEG_BGSUB", "CENT_BGSUB", 
-                              "N_MASKED_BGSUB", "P_MASKED_BGSUB"]):
-        
+                progress_bar=False, debug=False,):
+        t_init = time.time()
         # Bin out catalog based on the catalog in the configuration file
         binned_objects = galprime.bin_catalog(self.config)
         max_bins = len(binned_objects.bins) if max_bins is None else max_bins
@@ -59,56 +61,63 @@ class GPrime():
 
         if verbose:
             print("Running GalPRIME, version", galprime.__version__)
-            print(len(binned_objects.bins), "in total. Running GalPRIME on", max_bins)
+            print("-" + str(len(binned_objects.bins)), "bins in total. Running GalPRIME on", max_bins, "bins total")
+            if self.backgrounds is not None:
+                print("-Backgrounds were supplied:", len(self.backgrounds.cutouts), "in total.")
 
-
-        # Create all of our necessary filestructure
-        if not os.path.isdir(self.config["OUT_DIR"]):
-            os.mkdir(self.config["OUT_DIR"])
 
         # Generate the full filestructure, including containers for the individual profiles, and the medians
-        galprime.gen_filestructure(self.config["OUT_DIR"])
-        bare_dir = self.config["OUT_DIR"] + "bare_profiles/"
-        bgadded_dir = self.config["OUT_DIR"] + "bgadded_profiles/"
-        bgsub_dir = self.config["OUT_DIR"] + "bgsub_profiles/"
-        
-        bare_median_dir = self.config["OUT_DIR"] + "bare_medians/"
-        bgadded_median_dir = self.config["OUT_DIR"] + "bgadded_medians/"
-        bgsub_median_dir = self.config["OUT_DIR"] + "bgsub_medians/"
+        # The directories object contains all the required directories in a neat dict format
+        directories = galprime.gen_filestructure(self.config["OUT_DIR"])
 
-        additional_dir = self.config["OUT_DIR"] + "additional_info/"
-
-        
         # Run through bins and process using the method
         for i in range(max_bins):
             current_bin = binned_objects.bins[i]
-            
             columns = current_bin.columns()
-            
-            # Generate the object KDE that will be used by this bin
-            kde = galprime.object_kde(columns)
-                        
-            job_list, job_results = [], []
-            # Set up the job pool
-            print("Generating", self.config["N_MODELS"], "models for bin:", current_bin.bin_params)
-            with ProcessPool(max_workers=self.config["CORES"]) as pool:
-                for i in range(self.config["N_MODELS"]):
-                    job_list.append(pool.schedule(process_method,
-                                                args=(self, current_bin, kde),
-                                                timeout=self.config["TIME_LIMIT"]))
-            # Now get all the results
-            for i, job in enumerate(job_list):
-                try:
-                    job_results.append(job.result())
-                except Exception as error:
-                    print(error.args, i)
-            
-            prefix = galprime.gen_out_prefix(current_bin.bin_params)
             index_prefix = galprime.gen_index_prefix(current_bin.bin_params, self.config)
 
-            bare_hdulist = fits.HDUList()
-            bgadded_hdulist = fits.HDUList()
-            bgsub_hdulist = fits.HDUList()
+            
+            # Generate the object KDE that will be used by this bin, and save it to a pickle file 
+            kde = galprime.object_kde(columns)
+            galprime.save_pickle(kde, directories["ADDITIONAL"] + index_prefix + "_kde.dat")
+
+
+            # Set up the job pool
+            job_list, job_results = [], []
+            if verbose:
+                print("Working through", self.config["N_MODELS"], "models for bin:", current_bin.bin_params, 
+                      "with", self.config["CORES"], "cores." )
+            
+            # with ProcessPool(max_workers=self.config["CORES"]) as pool:
+            #     for i in range(self.config["N_MODELS"]):
+            #         job_list.append(pool.schedule(process_method,
+            #                                     args=(self, current_bin, kde),
+            #                                     timeout=self.config["TIME_LIMIT"]))
+            # # Now get all the results
+            # for i, job in enumerate(job_list):
+            #     try:
+            #         job_results.append(job.result())
+            #     except Exception as error:
+            #         print(" ", error.args, i)
+
+            def task_done(future):
+                try:
+                    result = future.result()  # blocks until results are ready
+                    job_results.append(result)
+                except TimeoutError as error:
+                    print("Function took longer than %d seconds" % error.args[1])
+                except Exception as error:
+                    print("Function raised %s" % error)
+                    if debug:
+                        print(error.traceback)  # traceback of the function
+
+
+            with ProcessPool(max_workers=self.config["CORES"]) as pool:
+                for index in range(self.config["N_MODELS"]):
+                    future = pool.schedule(process_method, args=(self, current_bin, kde),
+                                           timeout=self.config["TIME_LIMIT"])
+                    future.add_done_callback(task_done)
+
 
             # Clean the jobs of any that just so happened to fail and return junk profiles
             cleaned_containers = []
@@ -120,41 +129,57 @@ class GPrime():
                         break
                 if good:
                     cleaned_containers.append(container)
-            print("Profiles cleaned", len(job_results), len(cleaned_containers))
+            print(" . Profiles cleaned", len(job_results), len(cleaned_containers))
             job_results = cleaned_containers
-                    
-                        
+            
+
+            # Now save all of the data into nice outputs
+            bare_hdulist, bgadded_hdulist, bgsub_hdulist = fits.HDUList(), fits.HDUList(), fits.HDUList()
+     
             for i, container in enumerate(job_results):
                 bare_hdulist.append(fits.BinTableHDU(data=galprime.table_from_isolist(container.model_profile)))
                 bgadded_hdulist.append(fits.BinTableHDU(data=galprime.table_from_isolist(container.bgadded_profile)))
                 bgsub_hdulist.append(fits.BinTableHDU(data=galprime.table_from_isolist(container.bgsub_profile)))
 
             try:
-                bare_hdulist.writeto(bare_dir + index_prefix + "bare.fits", overwrite=True)
-                bgadded_hdulist.writeto(bgadded_dir + index_prefix + "bgadded.fits", overwrite=True)
-                bgsub_hdulist.writeto(bgsub_dir + index_prefix + "bgsub.fits", overwrite=True)
+                bare_hdulist.writeto(directories["BARE_PROFILES"] + index_prefix + "bare.fits", overwrite=True)
+                bgadded_hdulist.writeto(directories["BGADDED_PROFILES"] + index_prefix + "bgadded.fits", overwrite=True)
+                bgsub_hdulist.writeto(directories["BGSUB_PROFILES"] + index_prefix + "bgsub.fits", overwrite=True)
             except Exception as error:
-                print("Error Generating Data Products (Not good!):", error.args)
+                print(" . Error Generating Data Products (Not good!):", error.args)
 
             try:
                 bare_profiles = [galprime.table_from_isolist(container.model_profile) for container in job_results]
                 bare_median_table = galprime.boostrap_median(bare_profiles)
-                bare_median_table.write(bare_median_dir + index_prefix + "medians.fits", overwrite=True)
+                bare_median_table.write(directories["BARE_MEDIANS"] + index_prefix + "medians.fits", overwrite=True)
             except Exception as error:
-                print("Error Bootstrapping Bare:", error.args)
+                print(" . Error Bootstrapping Bare:", error.args)
             try:    
                 bgadded_profiles = [galprime.table_from_isolist(container.bgadded_profile) for container in job_results]
                 bgadded_median_table = galprime.boostrap_median(bgadded_profiles)
-                bgadded_median_table.write(bgadded_median_dir + index_prefix + "medians.fits", overwrite=True)
+                bgadded_median_table.write(directories["BGADDED_MEDIANS"] + index_prefix + "medians.fits", overwrite=True)
             except Exception as error:
-                print("Error Bootstrapping Bgadded:", error.args)
+                print(" . Error Bootstrapping Bgadded:", error.args)
             try:
                 bgsub_profiles = [galprime.table_from_isolist(container.bgsub_profile) for container in job_results]
                 bgsub_median_table = galprime.boostrap_median(bgsub_profiles)
-                bgsub_median_table.write(bgsub_median_dir + index_prefix + "medians.fits", overwrite=True)
+                bgsub_median_table.write(directories["BGSUB_MEDIANS"] + index_prefix + "medians.fits", overwrite=True)
             except Exception as error:
-                print("Error Bootstrapping Bgsub:", error.args)
+                print(" . Error Bootstrapping Bgsub:", error.args)
 
+            try:
+                output_datatable = galprime.construct_infotable(job_results, self.table_names)
+                output_datatable.write(directories["ADDITIONAL"] + index_prefix + "_data.fits", 
+                                    format="fits", overwrite=True)
+            except Exception as error:
+                print(" . Error with saving the output table (compare metadata with table_names):", error.args)
+
+            if verbose:
+                print("Finished. Time elapsed:", np.round((time.time() - t_init) / 60, 3), "minutes.")
+
+            # ONLY in debugging, you can return the GalPrimeContainer list for the first bin.
+            if debug:
+                return job_list
 
 def gprime_single(gprime_obj, current_bin, kde):
     metadata = {}
@@ -174,8 +199,8 @@ def gprime_single(gprime_obj, current_bin, kde):
         psf = galprime.get_closest_psf(gprime_obj.psfs, metadata["RA"], metadata["DEC"])
 
     # Generate our container
-    container = galprime.GalPrimeContainer(config=gprime_obj.config, kde=kde, psf=psf,
-                                    background_cutout=gprime_obj.backgrounds.cutouts[bg_index], 
+    container = galprime.GalPrimeContainer(config=gprime_obj.config, kde=kde, psf=np.copy(psf),
+                                    background_cutout=np.copy(gprime_obj.backgrounds.cutouts[bg_index]), 
                                     metadata=metadata)
-    container.process_object(plot=True)
+    container.process_object(plot=False)
     return container
